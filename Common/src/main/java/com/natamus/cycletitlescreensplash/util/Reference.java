a4226fbb570cc3f5af9de5f9481ea21833850f5e
Rick South <rick@natamus.com>
2024-08-09 08:27:57 +0200

    Cycle Title Screen Splash source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/cycletitlescreensplash/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.cycletitlescreensplash.util;
+
+public class Reference {
+	public static final String MOD_ID = "cycletitlescreensplash";
+	public static final String NAME = "Cycle Title Screen Splash";
+	public static final String VERSION = "1.3";
+	public static final String ACCEPTED_VERSIONS = "[1.21.1]";
+}
